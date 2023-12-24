6c79964f923cea504e8ada6a56cedef508021aec
Olakunle64 <olakunleisiaq@gmail.com>
2023-12-24 15:23:57 +0000

    add more

--- MagicExtractor/extractor.py
+++ MagicExtractor/extractor.py
@@ -3,8 +3,10 @@
 	to an email and phone number from a text"""
 
 import re
-from website_text import pasted_text
+# from website_text import pasted_text
 
+with open("software_enginer.txt", "r", encoding="utf-8") as f:
+    pasted_text = f.read()
 
 def email_extractor():
     """A regex to extract email from a text"""
@@ -34,12 +36,18 @@ if __name__ == "__main__":
     matches_email = []
     phone_regex = phone_number_extractor()
     email_regex = email_extractor()
+    """
     for groups in phone_regex.findall(pasted_text):
         phone_number = "-".join([groups[0], groups[1], groups[2]])
         if groups[3] != "":
             phone_number += ' x' + groups[3]
         matches_phone_number.append(phone_number)
+    """
+    with open("software_enginer.txt", "r", encoding="utf-8") as f:
+        pasted_text = f.read()
     for groups in email_regex.findall(pasted_text):
         matches_email.append(groups[0])
-    print(matches_phone_number)
+    #print(matches_phone_number)
     print(matches_email)
+    with open("email_SE_scraped", "w") as f:
+        json.dump(matches_email, f)
